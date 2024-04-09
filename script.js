5c015813bb5d80a2d15c0bf6e3f354e1b5c6d779
Dat <ntddatj03@gmail.com>
2024-04-09 16:16:20 +0700

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const containerAcc = document.getElementById("container-account");
-const registerBtn = document.getElementById("register");
-const loginBtn = document.getElementById("login");
+const containerAcc = document.querySelector("#container-account");
+const registerBtn = document.querySelector("#register");
+const loginBtn = document.querySelector("#login");
 
 registerBtn.addEventListener("click", () => {
     containerAcc.classList.add("active");
@@ -10,55 +10,32 @@ loginBtn.addEventListener("click", () => {
     containerAcc.classList.remove("active");
 });
 
-function showPassword() {
-    const signupPassword = document.getElementById("signup-password");
-    const signinPassword = document.getElementById("signin-password");
-    const signupPasswordI = document.getElementById("signup-password-icon");
-    const signinPasswordI = document.getElementById("signin-password-icon");
+const password = document.querySelectorAll(".password");
+const passwordI = document.querySelectorAll(".password-icon");
 
-    if (signupPassword.type === "password" & signinPassword.type === "password") {
-        signupPassword.type = "text";
-        signupPasswordI.classList.remove("bi-eye-slash");
-        signupPasswordI.classList.add("bi-eye")
-
-        signinPassword.type = "text";
-        signinPasswordI.classList.remove("bi-eye-slash");
-        signinPasswordI.classList.add("bi-eye")
-    } else {
-        signupPassword.type = "password";
-        signupPasswordI.classList.remove("bi-eye");
-        signupPasswordI.classList.add("bi-eye-slash");
-
-        signinPassword.type = "password";
-        signinPasswordI.classList.remove("bi-eye");
-        signinPasswordI.classList.add("bi-eye-slash");
-    }
+for (let i = 0; i < passwordI.length; i++) {
+    passwordI[i].addEventListener('click', () => {
+        if (password[i].type === 'password') {
+            password[i].type = "text";
+            passwordI[i].classList.remove("bi-eye-slash");
+            passwordI[i].classList.add("bi-eye");
+        } else {
+            password[i].type === "password";
+            passwordI[i].classList.remove("bi-eye");
+            passwordI[i].classList.add("bi-eye-slash");
+        }
+    })
 };
 
-function showCPassword() {
-    const cPassword = document.getElementById("confirm-password");
-    const cPasswordI = document.getElementById("confirm-password-icon");
+// function checkPassword() {
+//     const checkPassword = document.getElementsByClassName("signup-password").value;
+//     const checkcPassword = document.getElementsByClassName("confirm-password").value;
 
-    if (cPassword.type === "password") {
-        cPassword.type = "text";
-        cPasswordI.classList.remove("bi-eye-slash");
-        cPasswordI.classList.add("bi-eye")
-    } else {
-        cPassword.type = "password";
-        cPasswordI.classList.remove("bi-eye");
-        cPasswordI.classList.add("bi-eye-slash");
-    }
-};
-
-function checkPassword() {
-    const checkPassword = document.getElementsByClassName("signup-password").value;
-    const checkcPassword = document.getElementsByClassName("confirm-password").value;
-
-    if (checkPassword == checkcPassword) {
-        document.getElementById("warning").innerHTML = "Form is submitted";
-        return true;
-    } else {
-        document.getElementById("warning").innerHTML = "Password did not match";
-        return false;
-    }
-}
+//     if (checkPassword == checkcPassword) {
+//         document.getElementById("warning").innerHTML = "Form is submitted";
+//         return true;
+//     } else {
+//         document.getElementById("warning").innerHTML = "Password did not match";
+//         return false;
+//     }
+// }
